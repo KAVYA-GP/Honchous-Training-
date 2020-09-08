6233e66dca0442dbf8c0b2c2dfec37c3ca59727c
KAVYA-GP <60704442+KAVYA-GP@users.noreply.github.com>
2020-09-08 14:56:38 +0530

    Add files via upload

---
+++ landing/main.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+	setTimeout(wrapper,1000);
+}
+function wrapper(){
+	var w=document.querySelector(".wrapper");
+	w.style.opacity='1';
+}
+window.alert("You are in Home Page");
+
